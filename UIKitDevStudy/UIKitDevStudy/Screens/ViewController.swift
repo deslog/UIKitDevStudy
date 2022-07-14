fb0a9ce8694433dc3af5e7bea89b6a974db69860
JI SOO <dlwltn815@gmail.com>
2022-07-14 23:57:48 +0900

    [Chore] UIKitDevStudy 프로젝트 생성

---
+++ UIKitDevStudy/UIKitDevStudy/Screens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitDevStudy
+//
+//  Created by LeeJiSoo on 2022/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
