d81f90aa5857a5aae18b465ea616670873277b26
JI SOO <dlwltn815@gmail.com>
2022-07-15 17:38:54 +0900

    [Wip] 테이블뷰 생성

---
+++ UIKitDevStudy/UIKitDevStudy/Screens/HitTest/HitTestViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HitTestViewController.swift
+//  UIKitDevStudy
+//
+//  Created by LeeJiSoo on 2022/07/15.
+//
+
+import UIKit
+
+// ref: https://www.youtube.com/watch?v=2yVzeFIMtyc
+class HitTestViewController: UIViewController {
+    
+    private tableView: UITableView = {
+        
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
