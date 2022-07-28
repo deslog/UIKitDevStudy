18d3a9f14d1838b189bd6013b2b2ae5f71ab6439
JI SOO <dlwltn815@gmail.com>
2022-07-28 12:30:55 +0900

    [Feat] foreground상황에서 알림 오도록 extension추가

--- UIKitDevStudy/UIKitDevStudy/Global/Support/AppDelegate.swift
+++ UIKitDevStudy/UIKitDevStudy/Global/Support/AppDelegate.swift
@@ -32,3 +32,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner])
+    }
+}
