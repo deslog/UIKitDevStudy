02ad61c1951deffb39a782e61c4c47c252fdf40f
JI SOO <dlwltn815@gmail.com>
2022-08-09 18:49:38 +0900

    [Chore] xcode에러 해결 못해서 그냥 New로 생성
    - 나중에 해결해보자 일단급하니까!

---
+++ UIKitDev_New/UIKitDev_New/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitDev_New
+//
+//  Created by LeeJiSoo on 2022/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
