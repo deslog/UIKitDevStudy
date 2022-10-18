9064b513dfa1beb3f9e9bb6ad1440b36e4476815
JI SOO <dlwltn815@gmail.com>
2022-10-18 23:10:23 +0900

    [Wip] collectionview 생성

---
+++ UIKitDev_New/UIKitDev_New/Screens/DynamicCollectionView/DynamicCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  DynamicCollectionViewCell.swift
+//  UIKitDev_New
+//
+//  Created by 이지수 on 2022/10/18.
+//
+
+import UIKit
+
+class DynamicCollectionViewCell: UICollectionViewCell {
+
+    // MARK: properties
+
+    static let identifier = "DynamicCollectionViewCell"
+
+    // MARK: view
+
+    private lazy var newsTitleLabel: UILabel = {
+        $0.text = "우리는 왜 '타다'에 선뜻 타지 못했을까?"
+        $0.font = UIFont.preferredFont(forTextStyle: .title3)
+        $0.numberOfLines = 0
+        return $0
+    }(UILabel())
+
+    private lazy var newsCategoryLabel: UILabel = {
+        $0.text = "시사"
+        $0.font = UIFont.preferredFont(forTextStyle: .caption2)
+        return $0
+    }(UILabel())
+
+    // MARK: method
+
+    func setupLayout() {
+        [newsTitleLabel, newsCategoryLabel].forEach { addSubview($0) }
+        NSLayoutConstraint.activate([
+            newsTitleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 14.0),
+            newsTitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 14.0),
+            newsTitleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -14.0),
+
+            newsCategoryLabel.leadingAnchor.constraint(equalTo: newsTitleLabel.leadingAnchor),
+            newsCategoryLabel.trailingAnchor.constraint(equalTo: newsTitleLabel.trailingAnchor),
+            newsCategoryLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15.0)
+        ])
+    }
+}
--- UIKitDev_New/UIKitDev_New/Screens/DynamicCollectionView/DynamicCollectionViewController.swift
+++ UIKitDev_New/UIKitDev_New/Screens/DynamicCollectionView/DynamicCollectionViewController.swift
@@ -5,15 +5,73 @@
 //  Created by 이지수 on 2022/10/18.
 //
 
+// ipadOS 로 해야함
+
 import UIKit
 
 class DynamicCollectionViewController: UIViewController {
 
+    // MARK: view
+
+    private let collectionView: UICollectionView = {
+        $0.backgroundColor = .red
+        return $0
+    }(UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()))
+
+
+    // MARK: life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+        setupLayout()
+    }
 
+    // MARK: private func
+
+    private func configureCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(DynamicCollectionViewCell.self, forCellWithReuseIdentifier: DynamicCollectionViewCell.identifier)
+    }
+
+    private func setupLayout() {
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
 
+// MARK: collectionView delegate
 
+extension DynamicCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width - 36
+        return CGSize(width: width, height: 380.0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 100.0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let value = 16.0
+        return UIEdgeInsets(top: value, left: value, bottom: value, right: value)
+    }
+}
+
+extension DynamicCollectionViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DynamicCollectionViewCell.identifier, for: indexPath) as? DynamicCollectionViewCell
+        cell?.setupLayout()
+        return cell ?? UICollectionViewCell()
+    }
+}
