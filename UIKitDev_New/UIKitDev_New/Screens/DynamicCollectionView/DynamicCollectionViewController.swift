21446780c5e1fb51b17bba6bdcfea7759773ef88
JI SOO <dlwltn815@gmail.com>
2022-10-18 19:03:20 +0900

    [Move] 파일 이동 및 폴더정리

---
+++ UIKitDev_New/UIKitDev_New/Screens/DynamicCollectionView/DynamicCollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DynamicCollectionViewController.swift
+//  UIKitDev_New
+//
+//  Created by 이지수 on 2022/10/18.
+//
+
+import UIKit
+
+class DynamicCollectionViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+
