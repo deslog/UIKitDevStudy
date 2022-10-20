81e20e1c18a86099be3b7792dacee3cd24f32e4a
JI SOO <dlwltn815@gmail.com>
2022-10-20 23:26:06 +0900

    [Add] 동적으로 움직이는 cell 적용
    - cell의 길이가 동적으로 움직이게 설정

--- UIKitDev_New/UIKitDev_New/Screens/DynamicCollectionView/CollectionViewCell.swift
+++ UIKitDev_New/UIKitDev_New/Screens/DynamicCollectionView/CollectionViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  DynamicCollectionViewCell.swift
+//  CollectionViewCell.swift
 //  UIKitDev_New
 //
 //  Created by 이지수 on 2022/10/18.
@@ -11,12 +11,19 @@ class CollectionViewCell: UICollectionViewCell {
 
     // MARK: properties
 
-    static let identifier = "DynamicCollectionViewCell"
+    static let identifier = "CollectionViewCell"
 
     // MARK: view
 
-    private lazy var newsTitleLabel: UILabel = {
+    private lazy var newsBackgroundView: UIView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .blue
+        return $0
+    }(UIView())
+
+    var newsTitleLabel: UILabel = {
         $0.text = "우리는 왜 '타다'에 선뜻 타지 못했을까?"
+        $0.numberOfLines = 0
         $0.backgroundColor = .yellow
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.font = UIFont.preferredFont(forTextStyle: .title3)
@@ -35,15 +42,21 @@ class CollectionViewCell: UICollectionViewCell {
     // MARK: method
 
     func setupLayout() {
-        [newsTitleLabel, newsCategoryLabel].forEach { addSubview($0) }
+        [newsBackgroundView ,newsTitleLabel, newsCategoryLabel].forEach { addSubview($0) }
         NSLayoutConstraint.activate([
-            newsTitleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 14.0),
-            newsTitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 14.0),
-            newsTitleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -14.0),
+            newsBackgroundView.topAnchor.constraint(equalTo: self.topAnchor),
+            newsBackgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            newsBackgroundView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            newsBackgroundView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
 
+            newsTitleLabel.topAnchor.constraint(equalTo: newsBackgroundView.topAnchor, constant: 14.0),
+            newsTitleLabel.leadingAnchor.constraint(equalTo: newsBackgroundView.leadingAnchor, constant: 14.0),
+            newsTitleLabel.trailingAnchor.constraint(equalTo: newsBackgroundView.trailingAnchor, constant: -14.0),
+
+            newsCategoryLabel.topAnchor.constraint(equalTo: newsTitleLabel.bottomAnchor, constant: 50),
             newsCategoryLabel.leadingAnchor.constraint(equalTo: newsTitleLabel.leadingAnchor),
             newsCategoryLabel.trailingAnchor.constraint(equalTo: newsTitleLabel.trailingAnchor),
-            newsCategoryLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15.0)
+            newsCategoryLabel.bottomAnchor.constraint(equalTo: newsBackgroundView.bottomAnchor, constant: -15.0)
         ])
     }
 }
--- UIKitDev_New/UIKitDev_New/Screens/DynamicCollectionView/CollectionViewController.swift
+++ UIKitDev_New/UIKitDev_New/Screens/DynamicCollectionView/CollectionViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 
 class CollectionViewController: UIViewController {
 
+    let letters = ["안녕","안녕하세요","안녕하세요 저는 포마입니다.", "asdfasdasdfasdfasdfasdfasdfasdfasdfacwerfcaesdfasdfasdfasdfaasdfasdfasdfasdfasdfasdfafasdfas","안녕하세요 만나서 정말 반갑습니다."]
+
     // MARK: view
 
     private let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
+        let layout = CollectionViewLeftAlignFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.backgroundColor = .systemPink
@@ -33,9 +35,14 @@ class CollectionViewController: UIViewController {
     // MARK: private func
 
     private func configureCollectionView() {
+
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.identifier)
+        collectionView.collectionViewLayout = CollectionViewLeftAlignFlowLayout()
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.identifier)
     }
 
     private func setupLayout() {
@@ -51,36 +58,63 @@ class CollectionViewController: UIViewController {
 
 // MARK: collectionView delegate
 
-extension CollectionViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.frame.width
-        let numberOfItemsPerRow : CGFloat = 5
-        let spacing: CGFloat = 20
-        let availableWidth = width - spacing * (numberOfItemsPerRow + 1)
-        let itemDimension = floor(availableWidth / numberOfItemsPerRow)
+extension CollectionViewController: UICollectionViewDelegate {
 
-        return CGSize(width: itemDimension, height: 180)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
-    }
 }
+
+//extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let width = collectionView.frame.width
+//        let numberOfItemsPerRow : CGFloat = 5
+//        let spacing: CGFloat = 20
+//        let availableWidth = width - spacing * (numberOfItemsPerRow + 1)
+//        let itemDimension = floor(availableWidth / numberOfItemsPerRow)
+//
+//        return CGSize(width: itemDimension, height: 180)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 20
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 20
+//    }
+//}
 
 // MARK: datasource
 
 extension CollectionViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        return letters.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier, for: indexPath) as? CollectionViewCell
         cell?.setupLayout()
+        cell?.newsTitleLabel.text = letters[indexPath.item]
         return cell ?? UICollectionViewCell()
+    }
+}
+
+class CollectionViewLeftAlignFlowLayout: UICollectionViewFlowLayout {
+    let cellSpacing: CGFloat = 10
+
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        self.minimumLineSpacing = 10.0
+        self.sectionInset = UIEdgeInsets(top: 12.0, left: 16.0, bottom: 0.0, right: 16.0)
+        let attributes = super.layoutAttributesForElements(in: rect)
+
+        var leftMargin = sectionInset.left
+        var maxY: CGFloat = -1.0
+        attributes?.forEach { layoutAttribute in
+            if layoutAttribute.frame.origin.y >= maxY {
+                leftMargin = sectionInset.left
+            }
+            layoutAttribute.frame.origin.x = leftMargin
+            leftMargin += layoutAttribute.frame.width + cellSpacing
+            maxY = max(layoutAttribute.frame.maxY, maxY)
+        }
+        return attributes
     }
 }
