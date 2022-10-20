112f79d53290b0506583de4d1b3e532166fa981e
JI SOO <dlwltn815@gmail.com>
2022-10-21 01:14:29 +0900

    [Add] cell 레이아웃 수정

--- UIKitDev_New/UIKitDev_New/Screens/NewsFeeds/NewsFeedsViewController.swift
+++ UIKitDev_New/UIKitDev_New/Screens/NewsFeeds/NewsFeedsViewController.swift
@@ -120,15 +120,17 @@ extension NewsFeedsViewController: UICollectionViewDelegateFlowLayout {
 //        let spacing: CGFloat = 20
 //        let availableWidth = width - spacing * (numberOfItemsPerRow + 1)
 //        let itemDimension = floor(availableWidth / numberOfItemsPerRow)
-        let width = (collectionView.frame.width - (12 * 4)) / 5
+        var width: CGFloat
+        if data[indexPath.item].newsTitle.count > 30 {
+            width = ((collectionView.frame.width - (12 * 4)) / 5) * 2 + 12
+        } else {
+            width = (collectionView.frame.width - (12 * 4)) / 5
+        }
+
         return CGSize(width: width, height: 180)
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 12
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 12
     }
 }
